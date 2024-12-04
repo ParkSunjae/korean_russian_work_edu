f023227ab5271d8356bcb270e418f397d4e696ca
sean-pnpt <104343752+sean-pnpt@users.noreply.github.com>
2024-12-04 16:14:52 +0900

    게시자 정보 추가

--- src/app/api/ads/route.ts
+++ src/app/api/ads/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const adsContent = 'google.com, pub-XXXXXXXXXXXXXXXX, DIRECT, f08c47fec0942fa0';
+  const adsContent = 'google.com, pub-1590559582284803, DIRECT, f08c47fec0942fa0';
   
   return new NextResponse(adsContent, {
     headers: {
