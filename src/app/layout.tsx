b8f98fcbacd895ba4d1a21db54557e9267153b28
sean-pnpt <104343752+sean-pnpt@users.noreply.github.com>
2024-12-27 09:33:43 +0900

    구글 애드센스 태그 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import ClientLayout from "@/components/ClientLayout";
 import "./globals.css";
 
@@ -13,6 +14,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ko">
       <body className={inter.className}>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1590559582284803"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
